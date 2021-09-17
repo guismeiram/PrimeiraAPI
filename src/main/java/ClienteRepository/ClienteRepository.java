6c6e4fee72520b74b2ff844f4d7de5a7779ed597
guismeiram <boralagui@outlook.com>
2021-09-17 00:34:33 -0300

    commitando projeto

---
+++ src/main/java/ClienteRepository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package ClienteRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.Model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
